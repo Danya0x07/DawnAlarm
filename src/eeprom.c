8b80ddc4773dcee16765908ea6738a5c3cf6813e
Danya0x07 <dlef0xf8@gmail.com>
2020-10-14 13:29:14 +0300

    Настройки теперь сохраняются в nvram DS1307.

--- src/eeprom.c
+++ src/eeprom.c
@@ -1,4 +1,5 @@
 #include "eeprom.h"
+#include "rtc.h"
 
 #define AT24C32_ADDRESS 0b10100000
 
@@ -8,7 +9,8 @@ void eeprom_save(struct options *opts)
      * Поле __eeprom_addr воспринимается микросхемой как адрес, куда записывать
      * остальную часть структуры, т.е. фактически оно не осхраняется в eeprom.
      */
-    i2c_write_bytes(AT24C32_ADDRESS, (uint8_t *)opts, sizeof(*opts), 0);
+    //i2c_write_bytes(AT24C32_ADDRESS, (uint8_t *)opts, sizeof(*opts), 0);
+    rtc_access_nvram((uint8_t *)opts, sizeof(*opts), RTC_NVRAM_SAVE);
 }
 
 void eeprom_load(struct options *opts)
@@ -17,8 +19,9 @@ void eeprom_load(struct options *opts)
      * Сначала записываем адрес, откуда считывать, а далее считываем данные
      * в структуру, начиная с этого адреса.
      */
-    i2c_write_bytes(AT24C32_ADDRESS, (const uint8_t *)&opts->__eeprom_addr, 
+    /*i2c_write_bytes(AT24C32_ADDRESS, (const uint8_t *)&opts->__eeprom_addr, 
                     sizeof(opts->__eeprom_addr), I2C_NOSTOP);
     i2c_read_bytes(AT24C32_ADDRESS, (uint8_t *)opts + sizeof(opts->__eeprom_addr),
-                   sizeof(*opts) - sizeof(opts->__eeprom_addr), 0);
+                   sizeof(*opts) - sizeof(opts->__eeprom_addr), 0); */
+    rtc_access_nvram((uint8_t *)opts, sizeof(*opts), RTC_NVRAM_LOAD);
 }
